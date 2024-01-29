2a77057abb13c68cb955aba070f1c4ae74d86f4b
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-01-29 13:21:52 +0530

    added customer cart and product

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,9 +1,11 @@
 import {legacy_createStore,applyMiddleware, combineReducers} from 'redux'
 import {thunk} from 'redux-thunk'
 import {authReducer} from './auth/Reducer'
+import customerProductReducer from './Customers/Product/Reducer'
 
 const rootReducers = combineReducers({
 
-    auth:authReducer
+    auth:authReducer,
+    customersProduct:customerProductReducer,
 })
 export const store = legacy_createStore(rootReducers,applyMiddleware(thunk))
